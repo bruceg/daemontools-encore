5f8db65178d894ab539d1f48a06c6eee69aa4e87
Bruce Guenter <bruce@untroubled.org>
2010-06-24 15:05:46 -0600

    Fix the macro definitions in wait.h

--- wait.h
+++ wait.h
@@ -6,9 +6,9 @@
 extern int wait_pid(int *wstat,int pid);
 extern int wait_nohang(int *wstat);
 
-#define wait_crashed(w) ((w) & 127)
+#define wait_crashed(w) ((w) & 0x7f)
 #define wait_exitcode(w) ((w) >> 8)
-#define wait_stopsig(w) ((w) >> 8)
-#define wait_stopped(w) (((w) & 127) == 127)
+#define wait_stopsig(w) ((w) & 0x7f)
+#define wait_stopped(w) (((w) & 0x7f) == 0x7f)
 
 #endif
