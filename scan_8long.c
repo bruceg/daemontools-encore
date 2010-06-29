3e22fdff95c6a019349e6e7e686c86ceb89816d6
Bruce Guenter <bruce@untroubled.org>
2010-06-29 15:34:25 -0600

    Copy in scan_8long object from public domain ezmlm sources

---
+++ scan_8long.c
@@ -0,0 +1,12 @@
+#include "scan.h"
+
+unsigned int scan_8long(const char *s,unsigned long *u)
+{
+  unsigned int pos;
+  unsigned long result;
+  unsigned long c;
+  pos = 0; result = 0;
+  while ((c = (unsigned long) (unsigned char) (s[pos] - '0')) < 8)
+    { result = result * 8 + c; ++pos; }
+  *u = result; return pos;
+}
