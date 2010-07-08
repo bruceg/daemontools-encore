a83c38419cf5e27e80deb08eafdad847e1688c85
Bruce Guenter <bruce@untroubled.org>
2010-07-08 11:56:28 -0600

    Remove empty if statement in buffer_put.c

--- buffer_put.c
+++ buffer_put.c
@@ -15,7 +15,6 @@ static int allwrite(int (*op)(),int fd,const char *buf,unsigned int len)
       if (errno == error_intr) continue;
       return -1; /* note that some data may have been written */
     }
-    if (w == 0) ; /* luser's fault */
     buf += w;
     len -= w;
   }
