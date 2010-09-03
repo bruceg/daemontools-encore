ad0c88829a3b8a2ce45bbae771ec000528765b38
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2010-09-03 20:27:47 +0900

    rts.tests: Add /usr/ucb to $PATH to find printenv(1) on Solaris

--- rts.tests/00-preamble.sh
+++ rts.tests/00-preamble.sh
@@ -1,4 +1,4 @@
-PATH=`pwd`:/command:/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin
+PATH=`pwd`:/command:/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin:/usr/ucb
 export PATH
 
 umask 022
