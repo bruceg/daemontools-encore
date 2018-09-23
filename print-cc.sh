617bd4b918310422cfd65810ad3c5e236ef85f63
Rick Myers <rick@jrmyers.net>
2018-09-23 13:30:19 -0400

    trymemrchr.c typo and missing define

--- print-cc.sh
+++ print-cc.sh
@@ -2,4 +2,7 @@ cc="`head -1 conf-cc`"
 systype="`cat systype`"
 
 cat warn-auto.sh
-echo exec "$cc" '-c ${1+"$@"}'
+echo exec "$cc" -D_GNU_SOURCE '-c ${1+"$@"}'
+
+# Feature Test Macro Requirements for glibc (see feature_test_macros(7)):
+#   memrchr(): _GNU_SOURCE
