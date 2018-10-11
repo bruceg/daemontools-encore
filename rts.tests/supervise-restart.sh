b633bf9b859fe506f1ffb656d1913fc394e9d3ec
Bruce Guenter <bruce@untroubled.org>
2018-10-10 20:10:28 -0600

    rts: Add tests for proper service restart behavior
    Ensure that supervise does not restart programs that exit 100

---
+++ rts.tests/supervise-restart.sh
@@ -0,0 +1,27 @@
+rm -f test.sv/down
+catexe test.sv/start <<EOF
+#!/bin/sh
+echo started
+EOF
+
+for code in 0 1 100
+do
+    echo "--- supervise restarts service exiting $code"
+    catexe test.sv/run2 <<EOF
+#!/bin/sh
+echo rerunning
+exec sleep 999
+EOF
+    catexe test.sv/run <<EOF
+#!/bin/sh
+echo running
+mv run2 run
+exit $code
+EOF
+    supervise test.sv &
+    sleep 3
+    svc -xk test.sv
+    wait
+    echo
+done
+rm -f test.sv/start
